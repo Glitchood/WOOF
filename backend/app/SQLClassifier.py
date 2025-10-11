6aa5b1c82116b8afe2162946845b9ae84661067a
Kriv Jhajhria <105179164+Glitchood@users.noreply.github.com>
2025-10-11 12:06:52 -0700

    🤡 add sql vulnerability

---
+++ backend/app/SQLClassifier.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.pipeline import Pipeline, FeatureUnion
+import pickle
+
+filename = 'app/model/trained_model.pkl'
+with open(filename, 'rb') as file:
+  pipe = pickle.load(file)
+
+def predictSQL(s):
+  return pipe.predict(np.array([s])) == 1
